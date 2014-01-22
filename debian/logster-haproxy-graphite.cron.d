7aba1742508d1a6811a539250fb4609f0330b541
Brian Stengaard <brs@issuu.com>
2014-01-22 15:21:24 +0100

    run it as root #3837

--- debian/logster-haproxy-graphite.cron.d
+++ debian/logster-haproxy-graphite.cron.d
@@ -1,3 +1,3 @@
 # crontab for logster-haproxy-graphite
 # min        hour         day mon wday user     cmd
-1          19            *   *   *    logster  /usr/bin/graph-haproxy-metrics
+1          19            *   *   *    root  /usr/bin/graph-haproxy-metrics

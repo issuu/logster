6b9496661e0f30fe713167031dff89cf24bf5b29
Simon Ellefsen <se@issuu.com>
2014-02-04 22:06:54 +0100

    More fixes

--- debian/logster-haproxy-graphite.cron.d
+++ debian/logster-haproxy-graphite.cron.d
@@ -1,3 +1,3 @@
 # crontab for logster-haproxy-graphite
-# min        hour         day mon wday user     cmd
-1          19            *   *   *    root  /usr/bin/graph-haproxy-metrics
+# min    hour     day mon wday user     cmd
+*/1      *        *   *   *    root     /usr/bin/graph-haproxy-metrics
